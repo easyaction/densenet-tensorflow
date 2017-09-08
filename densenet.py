e9762290af9fc985eb5d7019f74764558c25b6a4
easyaction <ksy6808@gmail.com>
2017-09-08 16:11:50 +0900

    add data_loader from ikhlestov's densenet-tensorflow @easyaction

--- densenet.py
+++ densenet.py
@@ -3,9 +3,11 @@ import tensorflow as tf
 
 from layer import *
 
-class DenseNet:
-    def __init__(self):
-        pass
+class DenseNet(object):
+    def __init__(self,batch_size, num_classes, keep_prob):
+        self.batch_size = batch_size
+        self.num_classes = num_classes
+        self.keep_prob = keep_prob
 
     def bottleneck_layer(self,input_tensor, in_channels, out_channels, name, keep_prob, is_training=True):
         with tf.variable_scope(name):
@@ -38,7 +40,8 @@ class DenseNet:
             output_t = tf.contrib.layers.batch_norm(input_tensor, scale=True, is_training=is_training,
                                                     updates_collections=None)
             output_t = tf.nn.relu(output_t)
-            output_t = global_avg_pool(output_t)
+            last_pool_k = int(output_t.get_shape()[-2])
+            output_t = tf.nn.avg_pool(output_t,ksize=[1,last_pool_k,last_pool_k,1])
             output_t = tf.reshape(output_t, shape=[-1,num_classes])
             w_fc = weight_variable(output_t.get_shape().tolist(),name="fc")
             b_fc = bias_variable(num_classes,name="fc")
@@ -56,29 +59,28 @@ class DenseNet:
                 in_channels += k
         return output_t, in_channels
 
-    def build_networks(self,input_tensor, num_classes,growth_rate=12,is_training=True, name="densenet"):
+    def build_densenet(self,input_tensor, num_classes, growth_rate=12,is_training=True,keep_prob=self.keep_prob, name="densenet"):
         in_channels = input_tensor.get_shape()[3]
 
         output_t = conv2d(input_tensor, [7, 7, in_channels, 16], strides=2, padding="SAME", name="conv0")
         output_t = tf.nn.max_pool(output_t, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="VALID", name="max_pool0")
 
-        output_t, out_channels = self.dense_block(output_t, l=6, k=growth_rate, keep_prob=0.2, name="denseB_%d" % 1, is_training=is_training)
+        output_t, out_channels = self.dense_block(output_t, l=6, k=growth_rate, keep_prob=keep_prob, name="denseB_%d" % 1, is_training=is_training)
         output_t = self.transition_layer(output_t, in_channels=out_channels, c_rate=0.5, name="transL_%d" % 1, is_training=is_training)
 
-        output_t, out_channels = self.dense_block(output_t, l=12, k=growth_rate, keep_prob=0.2, name="denseB_%d" % 2, is_training=is_training)
+        output_t, out_channels = self.dense_block(output_t, l=12, k=growth_rate, keep_prob=keep_prob, name="denseB_%d" % 2, is_training=is_training)
         output_t = self.transition_layer(output_t, in_channels=out_channels, c_rate=0.5, name="transL_%d" % 2, is_training=is_training)
 
-        output_t, out_channels = self.dense_block(output_t, l=24, k=growth_rate, keep_prob=0.2, name="denseB_%d" % 3, is_training=is_training)
+        output_t, out_channels = self.dense_block(output_t, l=24, k=growth_rate, keep_prob=keep_prob, name="denseB_%d" % 3, is_training=is_training)
         output_t = self.transition_layer(output_t, in_channels=out_channels, c_rate=0.5, name="transL_%d" % 3, is_training=is_training)
 
-        output_t, out_channels = self.dense_block(output_t, l=16, k=growth_rate, keep_prob=0.2, name="denseB_%d" % 4, is_training=is_training)
+        output_t, out_channels = self.dense_block(output_t, l=16, k=growth_rate, keep_prob=keep_prob, name="denseB_%d" % 4, is_training=is_training)
 
-        logits = self.classification_layer(output_t,num_classes=num_classes,name="classL",keep_prob=0.2,is_training=is_training)
+        logits = self.classification_layer(output_t,num_classes=num_classes,name="classL",keep_prob=keep_prob,is_training=is_training)
 
         return logits
 
-
-
-
+    def build_loss(self,output, target, learning_rate,nesterov_momentum, weight_decay ):
+        pass
 
 
---
+++ loader.py
@@ -0,0 +1,98 @@
+import os
+import glob
+import collections
+import cv2
+import numpy as np
+import tensorflow as tf
+import shutil
+import math
+
+BatchTuple = collections.namedtuple("BatchTuple", ['images', 'labels'])
+
+
+def split_dataset(data_path, target_path, ratio):
+    shutil.rmtree(target_path, ignore_errors=True)
+    os.mkdir(target_path)
+
+    print("...Split from %s" % data_path)
+
+    dir_name_list = os.listdir(data_path)
+    for dir_name in dir_name_list:
+
+        dir_path = os.path.join(data_path, dir_name)
+        os.mkdir(os.path.join(target_path, dir_name))
+        file_name_list = os.listdir(dir_path)
+        print("\tNumber of files in %s = %d" % (dir_name, len(file_name_list)))
+
+        threshold_idx = math.ceil(len(file_name_list) * ratio)
+        split_file_name_list = file_name_list[:threshold_idx]
+
+        for file_name in split_file_name_list:
+            file_path = os.path.join(dir_path, file_name)
+            os.symlink(file_path, os.path.join(target_path, dir_name, file_name))
+    print("...Split done.")
+
+
+class Loader:
+    RawDataTuple = collections.namedtuple("RawDataTuple", ['path', 'label'])
+
+    def __init__(self, data_path, default_batch_size, image_info):
+        self.sess = tf.Session()
+        self.image_info = image_info
+        self.data_path = data_path
+        self.batch_size = default_batch_size
+
+        self.data = []
+        self.default_batch_size = default_batch_size
+        self.cur_idx = 0
+        self.perm_idx = []
+        self.epoch_counter = 0
+
+        self.load_data()
+
+    def load_data(self):
+        # Load data from directory
+        print("...Loading from %s" % self.data_path)
+        dir_name_list = os.listdir(self.data_path)
+        for dir_name in dir_name_list:
+            dir_path = os.path.join(self.data_path, dir_name)
+            file_name_list = os.listdir(dir_path)
+            print("\tNumber of files in %s = %d" % (dir_name, len(file_name_list)))
+            for file_name in file_name_list:
+                file_path = os.path.join(dir_path, file_name)
+                self.data.append(self.RawDataTuple(path=file_path, label=int(dir_name)))
+        print("\tTotal number of data = %d" % len(self.data))
+        print("...Loading done.")
+        self.reset()
+        return
+
+    def reset(self):
+        self.cur_idx = 0
+        self.perm_idx = np.random.permutation(len(self.data))
+        self.epoch_counter += 1
+        return
+
+    def get_batch(self, batch_size=None):
+        if batch_size is None:
+            batch_size = self.default_batch_size
+
+        if (self.cur_idx + batch_size) > len(self.data):
+            self.reset()
+            return None
+
+        batch = BatchTuple(
+            images=np.zeros(
+                dtype=np.uint8,
+                shape=[batch_size, self.image_info.height, self.image_info.width, self.image_info.channel]
+            ),
+            labels=np.zeros(dtype=np.int32, shape=[batch_size])
+        )
+
+        for idx in range(batch_size):
+            single_data = self.data[self.perm_idx[self.cur_idx + idx]]
+            image = cv2.imread(single_data.path)
+            batch.images[idx, :, :, :] = image
+            batch.labels[idx] = single_data.label
+        self.cur_idx += batch_size
+
+        return batch
